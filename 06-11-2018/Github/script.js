c733b89b578bb023f5ae913e40f5b14b8876abbf
kevinwong197 <kevinwong1997@hotmail.com>
2018-06-13 12:37:45 -0400

    added github-card component

--- 06-11-2018/Github/script.js
+++ 06-11-2018/Github/script.js
@@ -1,3 +1,15 @@
+Vue.component('github-card', {
+    props: ['card'],
+    template: `
+<div class="card">
+    <img class="avatar" :src="card.avatar_url" />
+    <div class="name">name: {{card.login}}</div>
+    <div class="followers">followers: {{card.followers}}</div>
+    <div class="following">follwing: {{card.following}}</div>
+</div>
+`
+});
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -12,7 +24,14 @@ const app = new Vue({
                 const apiUrl = 'https://api.github.com/users/' + this.username;
                 axios.get(apiUrl).then(response => {
                     this.error = false;
-                    this.cardlist.unshift(response.data.login);
+                    console.log(response.data);
+                    this.cardlist.unshift({
+                        login: response.data.login,
+                        avatar_url: response.data.avatar_url,
+                        followers: response.data.followers,
+                        following: response.data.following,
+                        html_url: response.data.html_url
+                    });
                 }, err => {
                     this.error = true;
                     this.errormsg = err.response.statusText;
@@ -24,4 +43,5 @@ const app = new Vue({
         },
     }
 });
+
 
